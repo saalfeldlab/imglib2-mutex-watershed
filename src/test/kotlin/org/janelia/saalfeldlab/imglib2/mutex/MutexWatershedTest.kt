57221b0ce21f4188849870bba00a0e47f05e6ef5
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-09-25 13:23:02 -0400

    Add more complex 2D test

--- src/test/kotlin/org/janelia/saalfeldlab/imglib2/mutex/MutexWatershedTest.kt
+++ src/test/kotlin/org/janelia/saalfeldlab/imglib2/mutex/MutexWatershedTest.kt
@@ -1,7 +1,14 @@
 package org.janelia.saalfeldlab.imglib2.mutex
 
+import net.imglib2.RandomAccessibleInterval
+import net.imglib2.img.array.ArrayImgs
+import net.imglib2.type.numeric.IntegerType
+import net.imglib2.util.Intervals
+import net.imglib2.util.Util
+import net.imglib2.view.Views
 import org.junit.Assert
 import org.junit.Test
+import java.util.Arrays
 
 class MutexWatershedTest {
 
@@ -26,6 +33,76 @@ class MutexWatershedTest {
         val indices = with (UnionFindExtensions) { uf.label(4) }
 
         Assert.assertArrayEquals(longArrayOf(0, 0, 0, 3), indices)
+
+    }
+
+    @Test
+    fun `simple 2D test with 3 regions`() {
+        // expected labeling (ids may differ):
+        // 1 - 1 - 2 - 3
+        //
+        // 1 - 2 - 2 - 3
+        //
+        // 1 - 1 - 3 - 3
+
+        val groundTruthLabeling = longArrayOf(
+                1, 1, 2, 3,
+                1, 2, 2, 3,
+                1, 1, 3, 3)
+
+        val nodes = longArrayOf(
+                0, 1,  2,  3,
+                4, 5,  6,  7,
+                8, 9, 10, 11)
+        val dims = longArrayOf(6, 3)
+        val groundTruthImg = ArrayImgs.unsignedLongs(groundTruthLabeling, *dims)
+        val nodeIdImg = ArrayImgs.unsignedLongs(LongArray(groundTruthLabeling.size) { it.toLong() }, *dims)
+
+        val nearestNeighborEdges = listOf(
+                // horizontal
+                Pair(Pair(0, 1), 1.00), Pair(Pair(1,  2), 0.10), Pair(Pair( 2,  3), 0.20),
+                Pair(Pair(4, 5), 0.05), Pair(Pair(5,  6), 0.97), Pair(Pair( 6,  7), 0.15),
+                Pair(Pair(8, 9), 0.99), Pair(Pair(9, 10), 0.02), Pair(Pair(10, 11), 0.93),
+
+                // vertical
+                Pair(Pair(0, 4), 0.76), Pair(Pair(1, 5), 0.03), Pair(Pair(2,  6), 0.84), Pair(Pair(3,  7), 0.91),
+                Pair(Pair(4, 8), 0.84), Pair(Pair(5, 9), 0.07), Pair(Pair(6, 10), 0.13), Pair(Pair(7, 11), 0.77))
+
+        val longDistanceEdges = listOf(Pair(Pair(0, 11), 0.71), Pair(Pair(1, 6), 0.67), Pair(Pair(6, 11), 0.84))
+
+
+
+        val edges = EdgeArray().also { e -> nearestNeighborEdges.forEach { e.addEdge(it.first.first.toLong(), it.first.second.toLong()) } }
+        val edgeWeights = nearestNeighborEdges.map { it.second }.toDoubleArray()
+
+        Assert.assertEquals(
+                1,
+                MutexWatershed.computeMutexWatershedClustering(
+                        nodes.size,
+                        edges,
+                        EdgeArray(),
+                        edgeWeights,
+                        doubleArrayOf()).setCount())
+
+
+        val mutexEdges = EdgeArray().also { e -> longDistanceEdges.forEach { e.addEdge(it.first.first.toLong(), it.first.second.toLong()) } }
+        val mutexEdgeWeights = longDistanceEdges.map { it.second }.toDoubleArray()
+
+        val uf = MutexWatershed.computeMutexWatershedClustering(
+                nodes.size,
+                edges,
+                mutexEdges,
+                edgeWeights,
+                mutexEdgeWeights)
+
+        Assert.assertEquals(nodes.size.toLong(), uf.size())
+        Assert.assertEquals(groundTruthLabeling.distinct().size.toLong(), uf.setCount())
+
+        val labeled = with (UnionFindExtensions) { uf.label(nodes.size) }
+
+        val labelMapping = mapOf(Pair(labeled[0], 1L), Pair(labeled[2], 2L), Pair(labeled[3], 3L))
+        val relabeled = labeled.map { labelMapping[it]!! }.toLongArray()
+        Assert.assertArrayEquals(relabeled, groundTruthLabeling)
 
     }
 
